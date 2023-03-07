2b0a009800d88101341171db9a010d6fbcdfb7f9
lamzuong <lamdavid821@gmail.com>
2023-03-07 19:44:41 +0700

    call API goods, truck

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -2,4 +2,8 @@ const listGoods = [];
 for (let i = 0; i < 24; i++) {
   listGoods.push({ value: i + 1, label: i + 1 });
 }
-export default listGoods;
+const listTrucks = [];
+for (let i = 0; i < 24; i++) {
+  listTrucks.push({ value: 'Xe ' + (i + 1) + ' tấn', label: 'Xe ' + (i + 1) + ' tấn' });
+}
+export { listGoods, listTrucks };
